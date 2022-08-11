efd3a931e3fa2c01c43b2d003d2558866d258445
AlexRogalevich <lanfis5902@gmail.com>
2022-08-11 19:26:06 +0300

    Create card.component.ts

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+const showElement = {
+  myelement: false
+}
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  showElement = showElement;
+  
+  toggleElement() {
+
+    showElement.myelement = !showElement.myelement
+
+    // console.log("Переключение", showElement.myelement);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
